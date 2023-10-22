5740ece1d659859de12b3ecf446e2057b90c33e8
joaquin Valdez lom <arclom@MacBook-Pro.local>
2023-10-22 16:04:31 -0600

    controller

--- src/controllers/deviceController.js
+++ src/controllers/deviceController.js
@@ -5,7 +5,15 @@ const bodyParser = require('body-parser')
 exports.takePhoto = (req,res)=>{
    console.log("Taking Photo");
    const io = req.app.get('io');
-   console.log(req.user.houseId);
-   io.in(1).emit('device.takephoto');
+
+   const {houseId} = req.user.houseId;
+   console.log(houseId);
+   io.emit('device.takephoto');
+   res.status(200).send(true);
+}
+
+exports.watering = (req,res)=>{
+   console.log("Request Watering");
+   io.emit('device.watering');
    res.status(200).send(true);
 }

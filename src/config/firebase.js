fba6e30f02d08310f73aedbf80dd6e6ce06b3027
joaquin Valdez lom <arclom@MacBook-Pro.local>
2023-10-22 16:46:20 -0600

    firebase

--- src/config/firebase.js
+++ src/config/firebase.js
@@ -3,7 +3,7 @@ const firebase = require('firebase-admin');
 const serviceAccount = require('./watering.json');
 
 firebase.initializeApp({
-   credential: firebase.credential.cert(serviceAccount);
+   credential: firebase.credential.cert(serviceAccount)
 });
 
 module.exports = { firebase }

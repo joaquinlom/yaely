187e4604a83d43a887d9fcf0432975f29c25fe7b
joaquin Valdez lom <arclom@MacBook-Pro.local>
2023-10-22 15:58:32 -0600

    controller

--- src/controllers/deviceController.js
+++ src/controllers/deviceController.js
@@ -4,6 +4,8 @@ const bodyParser = require('body-parser')
 
 exports.takePhoto = (req,res)=>{
    console.log("Taking Photo");
-
+   const io = req.app.get('io');
+   console.log(req.user);
+   io.in(1).emit('image.send',dataToSend);
    res.status(200).send(true);
 }

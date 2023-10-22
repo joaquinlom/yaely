ead736656ab654565035a40defca09f1b9641b6c
joaquin Valdez lom <arclom@MacBook-Pro.local>
2023-10-22 15:47:37 -0600

    websockets

--- src/routes/device.js
+++ src/routes/device.js
@@ -2,6 +2,8 @@ var express = require('express');
 var router = express.Router();
 var auth = require('./auth-config')
 const {User} = require('../database/index');
+const passport = require('passport');
+
 
 router.post('/takePhoto',passport.authenticate('jwt', {session: false}),(req,res)=>{
    console.log("Taking Photo");

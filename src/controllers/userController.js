d81952b5f5f01dff78eddf33c738422b9c354314
joaquin Valdez lom <arclom@MacBook-Pro.local>
2023-11-05 18:27:12 -0700

    Crons

--- src/controllers/userController.js
+++ src/controllers/userController.js
@@ -1,6 +1,23 @@
 const request = require('request')
 const bodyParser = require('body-parser')
 const {User} = require('../database/index');
+const nodemailer = require('nodemailer')
+
+
+
+
+function makeid(length) {
+   let result = '';
+   const characters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
+   const charactersLength = characters.length;
+   let counter = 0;
+   while (counter < length) {
+     result += characters.charAt(Math.floor(Math.random() * charactersLength));
+     counter += 1;
+   }
+   return result;
+}
+
 
 exports.saveFirebaseToken = async (req,res)=>{
    
@@ -32,5 +49,51 @@ exports.saveFirebaseToken = async (req,res)=>{
       res.status(200).send(false);
    }
    
+
+}
+
+/**
+ * The user will request an invitation to your household
+ * this will not create an active user
+ */
+exports.sendInvite =  async (req,res)=>{
+   const {name,email} = req.body;
+   const code = makeId(5);
+   const transporter = nodemailer.createTransport({
+      service: 'gmail',
+      auth: {
+      user: 'jvaldez.lom@gmail.com,'
+      pass: 'kxhplgwyejqxdoje'
+      }
+      });
+   const mailOptions = {
+      from: 'UACJ Yaely Tesis',
+      to: email,
+      subject: `${req.user.name} te invita!`,
+      text: `Acepta la invitation haciendo click en el siguiente link http://146.190.132.120/api/accept?code=${code}`
+      };
+   
+   
+   try{
+      transporter.sendMail(mailOptions, ()=>{
+         var user = await User.create({
+            name: name,
+            email:  email,
+            activation_token: code,
+            active: 0,
+            houseId: req.user.house.id 
+          });
+      })
+     res.status(200).send(true);
+   }catch(e){
+      res.status(500).send(e);
+   }
+}
+
+/**
+ * Find the user invitation from the url.
+ * This will activate the user, 
+ */
+exports.acceptInvite = async (req,res)=>{
 
 }

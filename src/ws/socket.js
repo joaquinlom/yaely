e6d95cef110efcda1056710691b0b9f094299eb8
joaquin Valdez lom <arclom@MacBook-Pro.local>
2023-11-19 17:04:20 -0700

    fixes

--- src/ws/socket.js
+++ src/ws/socket.js
@@ -39,7 +39,7 @@ module.exports.listen = function (app, server, session) {
         io.emit('device.sendphoto',{id: arg.id, img: arg.img})
 
         Photo.create({
-          image: arg.img,
+          img: arg.img,
           userId: arg.id
         });
         
